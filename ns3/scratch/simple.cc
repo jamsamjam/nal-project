744677d4ceb7b2f5ad4ddc5eff3a649a541aa409
jamsamjam <andsunlit@gmail.com>
2026-03-05 12:22:38 +0100

    simple example

---
+++ ns3/scratch/simple.cc
@@ -0,0 +1,67 @@
+// n0 -------------- n1 -------------- n2
+//    point-to-point    point-to-point
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("SimpleTopology");
+
+int
+main(int argc, char* argv[])
+{
+    CommandLine cmd(__FILE__);
+    cmd.Parse(argc, argv);
+
+    Time::SetResolution(Time::NS);
+    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+
+    NodeContainer nodes;
+    nodes.Create(3);
+
+    NodeContainer n0n1(nodes.Get(0), nodes.Get(1));
+    NodeContainer n1n2(nodes.Get(1), nodes.Get(2));
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("1Gbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    NetDeviceContainer d0d1 = pointToPoint.Install(n0n1);
+    NetDeviceContainer d1d2 = pointToPoint.Install(n1n2);
+
+    InternetStackHelper stack; 
+    stack.Install(nodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer i0i1 = address.Assign(d0d1);
+    address.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer i1i2 = address.Assign(d1d2);
+
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+    UdpEchoServerHelper echoServer(9);
+    ApplicationContainer serverApps = echoServer.Install(nodes.Get(2));
+    serverApps.Start(Seconds(1));
+    serverApps.Stop(Seconds(10));
+
+    UdpEchoClientHelper echoClient(i1i2.GetAddress(1), 9);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(1));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
+    clientApps.Start(Seconds(2));
+    clientApps.Stop(Seconds(10));
+
+    Simulator::Stop(Seconds(11));
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
+}
